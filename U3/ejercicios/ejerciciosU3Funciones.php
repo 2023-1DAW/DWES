af27a4cad27626a4e681155dfc715db44226fc5d
seteruiz <mruiz29@educa.madrid.org>
2024-10-17 10:50:21 +0200

    17oct terminados ejercicios funciones

--- U3/ejercicios/ejerciciosU3Funciones.php
+++ U3/ejercicios/ejerciciosU3Funciones.php
@@ -63,6 +63,7 @@
     function cuentaVocales($cadena)
     {
         $contador = 0;
+        $cadena = strtolower($cadena);
         $vocales = ["a", "e", "i", "o", "u"];
         for ($i = 0; $i < strlen($cadena); $i++) {
             if (in_array($cadena[$i], $vocales)) {
@@ -71,19 +72,84 @@
         }
         return $contador;
     }
+    var_dump(cuentaVocales("hola adios A"));
+    var_dump(cuentaVocales("hz"));
     var_dump(cuentaVocales(""));
 
     echo "<h2>Ejercicio 6</h2>";
+    function aumentaODisminuye($numero, $boolean)
+    {
+        if ($boolean) {
+            return $numero + 1;
+        } else {
+            return $numero - 1;
+        }
+    }
+    var_dump(aumentaODisminuye(3, true));
+    var_dump(aumentaODisminuye(3, false));
 
     echo "<h2>Ejercicio 7</h2>";
+    function esPar($n)
+    {
+        if ($n % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+    var_dump(esPar(4));
+    var_dump(esPar(5));
 
     echo "<h2>Ejercicio 8</h2>";
+    function arrayAletatorio($tam, $min, $max)
+    {
+        for ($i = 0; $i < $tam; $i++) {
+            $array[$i] = rand($min, $max);
+        }
+        return $array;
+    }
+    var_dump(arrayAletatorio(7, -5, 5));
+    var_dump(arrayAletatorio(3, -100, 100));
 
     echo "<h2>Ejercicio 9</h2>";
+    function arrayPares($array)
+    {
+        $ret = [];
+        foreach ($array as $n) {
+            if ($n % 2 == 0) {
+                $ret[] = $n;
+            }
+        }
+        return $ret;
+    }
+    var_dump(arrayPares([1, 2, 1, 2, 3, 4, 5, 6, 7]));
+    var_dump(arrayPares([1, 3, 5, 7]));
+    var_dump(arrayPares([2, 4, 6, 8, 0]));
 
     echo "<h2>Ejercicio 10</h2>";
+    function digitos($num){
+        return strlen($num);
+    }
+    var_dump(digitos(12345));
+    var_dump(digitos(0));
 
     echo "<h2>Ejercicio 11</h2>";
+    define ("DK", 0.13);
+    function dk2eur($cant){
+        return $cant * DK;
+    }
+    function eur2dk($cant){
+        return $cant / DK;
+    }
+    echo "<p>100 euros son " . eur2dk(100) . " coronas danesas</p>";
+    echo "<p>100 coronas danesas son " . dk2eur(100) . " euros</p>";
+
+    echo "<h2>Ejercicio 12</h2>";
+    function modifica(&$numero, $cantidad){
+        $numero += $cantidad;
+    }
+    $a = 5;
+    modifica($a, 3);
+    var_dump($a);
 
     ?>
 </body>
