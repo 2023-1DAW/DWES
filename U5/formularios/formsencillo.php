87b5b6dc649e75e23543ac1d43ee2d06fc851900
seteruiz <mruiz29@educa.madrid.org>
2024-11-05 12:37:00 +0100

    5nov segunda hora

--- U5/formularios/formsencillo.php
+++ U5/formularios/formsencillo.php
@@ -1,10 +1,25 @@
 <?php
 
-$nombre = $edad = "";
+$nombre = $edad = $estado = $newsletter = "";
 $nombreErr = $edadErr = "";
 $errores = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+     var_dump($_POST);
+
+    //Para radiobuttons tengo que ver si existe la clave en $_POST
+    if (isset($_POST["estado"])) {
+        $estado = $_POST["estado"];
+    }
+
+    //Para checkboxes me basta con ver si existe la clave en $_POST
+    if (isset($_POST["news"])) {
+        $newsletter = $_POST["news"];
+    }
+
+
     //Aquí dentro hago la validación
     //Como el nombre es obligatorio: 
     if (!empty($_POST["name"])) {
@@ -43,10 +58,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario sencillito para cogerles cariño</title>
     <style>
-        input.err{
+        input.err {
             border-color: red;
         }
-        label.error{
+
+        label.error {
             color: red;
             font-size: small;
         }
@@ -56,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <form method="POST" action="./formsencillo.php"> <!-- esto se debe mejorar -->
         <label>Nombre *: </label>
-        <input 
+        <input
             type="text"
             name="name"
             value="<?php echo $nombre; ?>"
@@ -79,15 +95,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         ?>
         <br>
+
+        <label>Estado civil:</label><br>
+        <input type="radio"
+            name="estado"
+            value="soltero"
+            <?php if (isset($_POST["estado"]) && $_POST["estado"] == "soltero") echo "checked"; ?>>Soltero<br>
+        <input
+            type="radio"
+            name="estado"
+            value="casado"
+            <?php if (isset($_POST["estado"]) && $_POST["estado"] == "casado") echo "checked"; ?>>Casado<br>
+        <input
+            type="radio"
+            name="estado"
+            value="viudo"
+            <?php if (isset($_POST["estado"]) && $_POST["estado"] == "viudo") echo "checked"; ?>>Viudo<br>
+
+        <br>
+        <input type="checkbox" name="news" 
+        <?php if (!empty($newsletter)) echo "checked"; ?>>Quiero suscribirme a la newsletter<br>
         <input type="submit" name="enviar" value="Envío">
         <input type="reset">
     </form>
 
     <?php
-    if (!$errores) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !$errores) {
+        echo "<h2>Contenido del formulario rellenado correctamente</h2>";
         echo "<p>Te damos la bienvenida, $nombre. Tienes $edad años.</p>";
+        if (!empty($estado)) {
+            echo "<p>Tu estado civil es $estado</p>";
+        } else {
+            echo "<p>No has marcado estado civil.</p>";
+        }
+        if (!empty($newsletter)) {
+            echo "<p>Te has sucrito.</p>";
+        } else {
+            echo "<p>No te has suscrito.</p>";
+        }
     }
-
     ?>
 </body>
 
