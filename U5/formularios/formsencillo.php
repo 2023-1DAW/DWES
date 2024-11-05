31e653c079f26330b0e6e6186f37358f0cdf4176
seteruiz <mruiz29@educa.madrid.org>
2024-11-05 10:17:16 +0100

    5nov primera hora

--- U5/formularios/formsencillo.php
+++ U5/formularios/formsencillo.php
@@ -1,5 +1,38 @@
 <?php
-//Validación de datos
+
+$nombre = $edad = "";
+$nombreErr = $edadErr = "";
+$errores = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Aquí dentro hago la validación
+    //Como el nombre es obligatorio: 
+    if (!empty($_POST["name"])) {
+        //Sí ha rellenado el nombre
+        $nombre = $_POST["name"];
+    } else {
+        $nombreErr = "El nombre es obligatorio";
+        $errores = true;
+    }
+
+    //Igual con la edad
+    if (!empty($_POST["age"])) {
+        //Sí ha rellenado el nombre
+        $edad = $_POST["age"];
+    } else {
+        $edadErr = "La edad es obligatoria";
+        $errores = true;
+    }
+
+    //Mañana hago un array donde se guarden todos los nombres introducidos
+}
+
+//Comprobaciones en caso de que sea de tipo GET:
+//if (isset($_GET["name"])) {
+//    echo "<p>Hola {$_GET["name"]}.</p>";
+//}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,34 +42,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario sencillito para cogerles cariño</title>
+    <style>
+        input.err{
+            border-color: red;
+        }
+        label.error{
+            color: red;
+            font-size: small;
+        }
+    </style>
 </head>
 
 <body>
     <form method="POST" action="./formsencillo.php"> <!-- esto se debe mejorar -->
-        <label>Nombre: </label>
-        <input type="text" name="name"><br>
+        <label>Nombre *: </label>
+        <input 
+            type="text"
+            name="name"
+            value="<?php echo $nombre; ?>"
+            class="<?php echo empty($nombreErr) ? "" : "err"; ?>">
+        <?php
+        if (!empty($nombreErr)) {
+            echo "<label class='error'>$nombreErr</label>";
+        }
+        ?>
+        <br>
+        <label>Edad *: </label>
+        <input
+            type="number"
+            name="age"
+            value="<?php echo $edad; ?>"
+            class="<?php echo empty($edadErr) ? "" : "err"; ?>">
+        <?php
+        if (!empty($edadErr)) {
+            echo "<label class='error'>$edadErr</label>";
+        }
+        ?>
+        <br>
         <input type="submit" name="enviar" value="Envío">
         <input type="reset">
     </form>
 
     <?php
-
-    var_dump($_POST);
-    var_dump($_GET);
-
-    //var_dump($_SERVER);
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo "<p>He llegado a través del formulario.</p>";
-
-        echo "<p>El nombre introducido es: {$_POST['name']}</p>";
-        //La línea de arriba es similar a:
-        echo "<p>El nombre introducido es: " . $_POST['name'] . "</p>";
-
-        //Mañana hago un array donde se guarden todos los nombres introducidos
-    } else {
-        echo "<p>Primera visita.</p>";
+    if (!$errores) {
+        echo "<p>Te damos la bienvenida, $nombre. Tienes $edad años.</p>";
     }
-
 
     ?>
 </body>
